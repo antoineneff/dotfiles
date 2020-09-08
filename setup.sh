7c1aabf20999449976e01dc1d791ee1fb6c3842b
Antoine Neff <9216777+antoineneff@users.noreply.github.com>
2020-09-08 22:57:56 +0200

    Update setup.sh : do not disable media keys as they're used for spotify

--- setup.sh
+++ setup.sh
@@ -105,9 +105,6 @@ defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# Stop iTunes from responding to the keyboard media keys
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
-
 # Prevent Photos from opening automatically when devices are plugged in
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
